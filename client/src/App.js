9aa078d78dc29b4d60ef5ca377376c36cc60aae2
Deep Pravinbhai Lad <dp658583@dal.ca>
2023-11-15 21:31:45 -0400

    feat: Add routes foundation for role-based access

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/login.js';
-import RegisterPage from './components/register.js';
-import Listrestaurants from './components/listrestaurants.js';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/login.js";
+import RegisterPage from "./components/register.js";
+import Listrestaurants from "./components/listrestaurants.js";
+import Cookies from "js-cookie";
 
 function App() {
+  // get userId from cookies each page where you're required to send to server
+  const userId = Cookies.get("userId");
+
+  // fetch from localstorage. Set isOwner true on successful login
+  const isRestaurantOwner = localStorage.getItem("isOwner");
+
+  // fetch from localstorage. Set isOwner true on successful login
+  const isAdmin = localStorage.getItem("isAdmin");
+
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/listrestaurant" element={<Listrestaurants/>}/>
+        <Route path="/login" element={<LoginPage />} />            
+        {/* // Add all the pages below that are only accessible by Admin after successful login */}
+        {isAdmin && <></>}
+
+        {/* // Add all the pages below that are only accessible by Restaurant Owners after successful login */}
+        {isRestaurantOwner && <></>}
+
+        {/* // Add all the pages below that are only accessible after successful login  */}
+        {userId && (
+          <>
+            <Route path="/listrestaurant" element={<Listrestaurants />} />
+          </>
+        )}
+        <Route path="/*" element={<LoginPage />} />
       </Routes>
     </Router>
   );
