b17548ee59e1ba4a8e5fb5c32f558f13eb5795a9
athaker <ab570421@dal.ca>
2023-12-02 13:34:42 -0400

    modified ui of listrestaurants to pass it to reservation

--- Customer App/client/src/components/listrestaurants.js
+++ Customer App/client/src/components/listrestaurants.js
@@ -1,112 +1,139 @@
 import React, { useState, useEffect } from 'react';
-import RegisterPage from './register';
-import LoginPage from './login';
 import './listrestaurant.css';
 import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, Link } from 'react-router-dom';
 
 function Listrestaurants() {
-  const [restaurants, setRestaurants] = useState([]);
-  const [token, setToken] = useState('');
-  const navigate = useNavigate();
-
-  const API_ENDPOINT  = 'https://mlpjq9hwih.execute-api.us-east-1.amazonaws.com/dev'
-
-  const handleLogout = () => {
-    // Remove the token from cookies or state (depending on your implementation)
-    Cookies.remove('userId');
-    // Redirect to the login page or any other page after logout
-    navigate('/login');
-  };
-
-const ListrestaurantAPI = {
+    const [restaurants, setRestaurants] = useState([]);
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
   
-  getAllRestaurants: async () => {
-    try {
-      const response = await fetch(`${API_ENDPOINT}/items`, {
-        method: 'GET',
-        headers: {
-          'origin': 'http://localhost:3000',
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log('Data:', data); // Log the data received
-        return data;
-      } else {
-        throw new Error('Failed to fetch all restaurants data');
-      }
-    } catch (error) {
-      throw new Error(`Error fetching all restaurants data: ${error.message}`);
-    }
-  },
-
-};
-
-  useEffect(() => {
-    const fetchRestaurants = async () => {
-      try {
-        const data = await ListrestaurantAPI.getAllRestaurants();
-        setRestaurants(data || []);
-      } catch (error) {
-        console.error('Error fetching restaurants:', error);
-      }
+    const API_ENDPOINT = 'https://mlpjq9hwih.execute-api.us-east-1.amazonaws.com/dev';
+  
+    const handleLogout = () => {
+      Cookies.remove('userId');
+      navigate('/login');
     };
+  
+    const ListrestaurantAPI = {
+      getAllRestaurants: async () => {
+        try {
+          const response = await fetch(`${API_ENDPOINT}/items`, {
+            method: 'GET',
+            headers: {
+              'origin': 'http://localhost:3000',
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`,
+            },
+          });
+  
+          if (response.ok) {
+            const data = await response.json();
+            console.log('Data:', data);
+            return data;
+          } else {
+            throw new Error('Failed to fetch all restaurants data');
+          }
+        } catch (error) {
+          throw new Error(`Error fetching all restaurants data: ${error.message}`);
+        }
+      },
+    };
+  
+    useEffect(() => {
+      const fetchRestaurants = async () => {
+        try {
+          const data = await ListrestaurantAPI.getAllRestaurants();
+          setRestaurants(data || []);
+        } catch (error) {
+          console.error('Error fetching restaurants:', error);
+        }
+      };
+  
+      fetchRestaurants();
+    }, []);
+  
+    return (
+      <div>
+        <div>
+          <h2 style={{ backgroundColor:"#fff", color: "#000", padding:'10px'}}>List of Available Restaurants</h2>
+            {restaurants.map((restaurant) => (
+              <p key={restaurant.restaurant_id}>
+                <AccordionItem
+                  title={restaurant.restaurant_name}
+                  openHours={restaurant.open_hours}
+                  closeHours={restaurant.close_hours}
+                  address={restaurant.address_line}
+                  phone={restaurant.phone}
+                  status={restaurant.status}
+                  menu={restaurant.menu}
+                />
+              </p>
+            ))}
+        </div>
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </div>
+    );
+  }
+  
+  const AccordionItem = ({ title, openHours, closeHours, address, phone, status, menu }) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleAccordion = () => {
+      setIsOpen(!isOpen);
+    };
+    const navigate = useNavigate();
 
-    fetchRestaurants();
-  }, []);
+  const onReserveClick = (restaurantData) => {
+        // Implement the logic to handle the reservation
+        console.log("Reserving:", restaurantData);
+        navigate('/reservation', { state: { restaurantData } });
 
-  return (
-    <div>
-    <div>
-        <h2 style={{color:"#fff"}}>List of Restaurants</h2>
-        <ul style={{ color: "#fff"}}>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.restaurant_id}>
-            <p> {restaurant.restaurant_name}</p>
-          {restaurant.open_hours && <p>Open Hours: {restaurant.open_hours}</p>}
-          {restaurant.close_hours && <p>Close Hours: {restaurant.close_hours}</p>}
-          {restaurant.address_line && <p>Address: {restaurant.address_line}</p>}
-          {restaurant.phone && <p>Phone: {restaurant.phone}</p>}
-          {restaurant.status ? (
-            <p>Status: {restaurant.status === "open" ? "Open" : "Closed"}</p>
-          ) : (
-            <p>Status: Closed</p>
-          )}
-
-            {restaurant.menu && (
-            <div>
-              <h4>Menu</h4>
-              {Object.keys(restaurant.menu).map((cuisine) => (
-                <div key={cuisine}>
-                  <h4>{cuisine}</h4>
-                  <ul>
-                    {Object.keys(restaurant.menu[cuisine]).map((id) => (
-                      <li key={id}>
-                        <p>Menu Name: {restaurant.menu[cuisine][id].menu_name}</p>
-                        <p>Price: {restaurant.menu[cuisine][id].price}</p>
-                        <p>Quantity: {restaurant.menu[cuisine][id].quantity}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          )}
-          </li>
-        ))}
-        </ul>
-    </div>
-    <div>
-    <h2 style={{ color: "#fff" }}>List of Restaurants</h2>
-    <button onClick={handleLogout}>Logout</button>
-  </div>
-  </div>
-  );
-}
-
-export default Listrestaurants;
+        // You can navigate to a reservation page or perform other actions
+    };
+    return (
+      <div className="accordion-item">
+        <div className="accordion-header" onClick={toggleAccordion}>
+          <h3>{title}</h3>
+          <span style={{ fontSize: "40px"}}>{isOpen ? "-" : "+"}</span>
+        </div>
+        {isOpen && (
+          <div className="accordion-content">
+            {openHours && <p>Open Hours: {openHours}</p>}
+            {closeHours && <p>Close Hours: {closeHours}</p>}
+            {address && <p>Address: {address}</p>}
+            {phone && <p>Phone: {phone}</p>}
+            {status ? <p>Status: {status === "open" ? "Open" : "Closed"}</p> : <p>Status: Closed</p>}
+  
+            {menu && (
+              <div className="accordion-content">
+                <h2>Menu</h2>
+                {Object.keys(menu).map((cuisine) => (
+                  <div key={cuisine}>
+                    <h3>{cuisine}</h3>
+                    <ul>
+                      {Object.keys(menu[cuisine]).map((id) => (
+                        <li key={id}>
+                          <div>Menu Name: {menu[cuisine][id].menu_name}</div>
+                          <div>Price: {menu[cuisine][id].price}</div>
+                          <div>Quantity: {menu[cuisine][id].quantity}</div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            )}
+            
+            <button onClick={() => onReserveClick({ title, openHours, closeHours, address, phone, status, menu })}>
+            Reserve </button>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default Listrestaurants;
+  
