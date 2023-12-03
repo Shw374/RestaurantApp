a3c8d944149ead70b62658cd1bbcbb79672f330f
Deep Pravinbhai Lad <dp658583@dal.ca>
2023-12-02 23:27:45 -0400

    Prettify

--- Admin App/client/src/components/Dashboard.js
+++ Admin App/client/src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Grid, Container, Typography } from "@mui/material";
 import "./Dashboard.css";
-
 import DashboardCard from "./DashboardCard";
 
 const Dashboard = () => {
